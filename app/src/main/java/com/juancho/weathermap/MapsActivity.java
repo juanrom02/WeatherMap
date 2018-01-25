9cace6a855453e016401ce9309c48d72b146cd8c
juanrom02 <juanrom02@gmail.com>
2018-01-25 17:24:46 -0300

    Marker moves on Long Click
    It also shows the locality in the title of the marker. Doesn't work very well for some areas, but that's an API issue.

--- app/src/main/java/com/juancho/weathermap/MapsActivity.java
+++ app/src/main/java/com/juancho/weathermap/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.juancho.weathermap;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,11 +10,19 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String locality;
+    private Marker marker;
+    private MarkerOptions markerOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +34,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        marker = mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)));
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener(){
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                locality = getLocality(latLng);
+                markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .title(locality)
+                        .draggable(false);
+                marker.remove();
+                marker = mMap.addMarker(markerOptions);
+                marker.showInfoWindow();
+            }
+        });
+    }
+
+    public String getLocality(LatLng latLng){
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addressList = null;
+
+        try{
+            addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        if (addressList != null && addressList.size() > 0) {
+            return addressList.get(0).getLocality();
+        }else{
+            return "";
+        }
     }
 }
